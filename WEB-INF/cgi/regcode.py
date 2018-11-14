6ed049b9ef1aa02edff57cdd920534088910327d
Sanjeev Pandey <sanpande@adobe.com>
2018-11-15 01:58:13 +0530

    Pythin 3 Compatibility Added

--- WEB-INF/cgi/regcode.py
+++ WEB-INF/cgi/regcode.py
@@ -6,9 +6,11 @@ import hashlib
 import time
 import string
 import binascii
-import urllib2
+import urllib.request as u2
 import urllib
-from urllib2 import Request, urlopen, URLError
+import urllib.parse as uparse
+# sys.path.append('../')
+from urllib.request import Request, urlopen, URLError
 from pyquery import PyQuery
 from webbrowser import open_new_tab
 
@@ -21,23 +23,34 @@ ua = form.getvalue('UA')
 reggie_fqdn = form.getvalue('REG_FQDN')
 uuid_filename = 'uuid.txt'
 
+def new_hmac(secret_key, headinfo):
+    if sys.version_info[0] == 2:
+        return hmac.new(bytes(secret_key), headinfo, hashlib.sha1).digest()
+    else:
+        return hmac.new(bytes(secret_key, 'utf-8'), headinfo, hashlib.sha1).digest()
+
+
 def buildAuthHeader(refid):
 
 	public_key = pub
 	private_key = priv
-	print 'HEADER START\n\n'
+	print ('HEADER START\n\n')
 
 	uuidvalue = str(uuid4())
 
 	print(int(time.time()*1000))
 	header = 'POST requestor_id=%s, nonce=%s, signature_method=HMAC-SHA1, request_time=%s, request_uri=/regcode' %(refid, uuidvalue, int(time.time()*1000))
-	digest = hmac.new(private_key, header, hashlib.sha1).digest()
+	print (header)
+	#digest = hmac.new(private_key, header.encode('utf-8'), hashlib.sha1).digest()
+	digest = new_hmac(private_key, header.encode('utf-8'))
 	sig = binascii.b2a_base64(digest)[:-1]
-	print sig
-	header += ', public_key=%s, signature=%s' %(public_key, sig)
-	print header
+	print (sig)
+	sig2 = sig.decode('utf-8')
+	print(sig2)
+	header += ', public_key=%s, signature=%s' %(public_key, sig2)
+	print (header)
 
-	print "HEADER END \n\n"
+	print ("HEADER END \n\n")
 	return header
 
 def writeUUID(strUUID):
@@ -55,26 +68,26 @@ theheader = buildAuthHeader(requestor_id)
 
 #reggie_fqdn = "http://api.auth.adobe.com/reggie/v1/"
 add_args = {'deviceId':deviceId, 'uuid':getUUID()}
-data = urllib.urlencode(add_args)
+data = uparse.urlencode(add_args)
 url_hdr = ua
 # get regcode 
 url = str(reggie_fqdn) + requestor_id + str("/regcode")
-print url
-request = urllib2.Request(url, data)
+print (url)
+request = u2.Request(url, data.encode('utf-8'))
 request.add_header('User-agent', url_hdr)
 request.add_header('Authorization',theheader)
 # Sends the request and catches the response
 try:
-        response = urllib2.urlopen(request)
+        response = u2.urlopen(request)
         html = response.read()
         #print html
-except URLError, e:
-        print e.code
-        print e.reason
-        print e.read()
+except URLError as e:
+        print (e.code)
+        print (e.reason)
+        print (e.read())
 # parse the regcode from xml
 pq = PyQuery(html)
 tag = pq('code') # or     tag = pq('div.class')
 device_info = pq('info').text()
-print 'Regcode: '+tag.text()
-print 'device_info: '+device_info.split('\n\t *')[0]
+print ('Regcode: '+tag.text())
+print ('device_info: '+device_info.split('\n\t *')[0])
--- WEB-INF/cgi/theApp.py
+++ WEB-INF/cgi/theApp.py
@@ -6,9 +6,10 @@ import hashlib
 import time
 import string
 import binascii
-import urllib2
+import urllib.request
 import urllib
-from urllib2 import Request, urlopen, URLError
+import urllib.parse as uparse
+from urllib.request import Request, urlopen, URLError
 from pyquery import PyQuery
 from webbrowser import open_new_tab
 
@@ -18,35 +19,43 @@ SmartDevice
 Created by: Sanjeev Pandey | AUG 2018
 '''
 
-form = cgi.FieldStorage() 
-pub  = form.getvalue('PUBLIC_KEY')
-priv  = form.getvalue('PRIV_KEY')
-requestor_id = form.getvalue('REQID')
-deviceId = form.getvalue('DEVID')
-ua = form.getvalue('UA')
-sp_fqdn = form.getvalue('SP_FQDN')
-reggie_fqdn = form.getvalue('REG_FGQN')
+#form = cgi.FieldStorage() 
+pub  = "rfSbthMG7RvLSWkMCcU7H5hEhDFHA13N" #form.getvalue('PUBLIC_KEY')
+priv  = "IBx4ckmQGZALgn5P" #form.getvalue('PRIV_KEY')
+requestor_id = "fbc-fox" #form.getvalue('REQID')
+deviceId = "sanjeev" #form.getvalue('DEVID')
+ua = "Dalvik/2.1.0 (Linux; U; Android 6.0; Android SDK built for x86_64 Build/MASTER)" #form.getvalue('UA')
+sp_fqdn = "http://api.auth.adobe.com/api/v1/" #form.getvalue('SP_FQDN')
+reggie_fqdn = "http://api.auth.adobe.com/reggie/v1/" #form.getvalue('REG_FGQN')
 uuid_filename = 'uuid.txt'
 login_page = 'mvpdLoginPage.html'
-mrss_res = form.getvalue('RESID')
+mrss_res = "ngw" #form.getvalue('RESID')
+
+def new_hmac(secret_key, headinfo):
+    if sys.version_info[0] == 2:
+        return hmac.new(bytes(secret_key), headinfo, hashlib.sha1).digest()
+    else:
+        return hmac.new(bytes(secret_key, 'utf-8'), headinfo, hashlib.sha1).digest()
 
 def buildAuthHeader(refid):
 
 	public_key = pub
 	private_key = priv
-	print 'HEADER START\n\n'
+	print ('HEADER START\n\n')
 
 	uuidvalue = str(uuid4())
 
 	print(int(time.time()*1000))
 	header = 'POST requestor_id=%s, nonce=%s, signature_method=HMAC-SHA1, request_time=%s, request_uri=/regcode' %(refid, uuidvalue, int(time.time()*1000))
-	digest = hmac.new(private_key, header, hashlib.sha1).digest()
+	#digest = hmac.new(private_key, header, hashlib.sha1).digest()
+	digest = new_hmac(private_key, header.encode('utf-8'))
 	sig = binascii.b2a_base64(digest)[:-1]
-	print sig
-	header += ', public_key=%s, signature=%s' %(public_key, sig)
-	print header
+	#print (sig)
+	sig2 = sig.decode('utf-8')
+	header += ', public_key=%s, signature=%s' %(public_key, sig2)
+	print (header)
 
-	print "HEADER END \n\n"
+	print ("HEADER END \n\n")
 	return header
 
 def writeUUID(strUUID):
@@ -72,13 +81,13 @@ theheader = buildAuthHeader(requestor_id)
 url_header = ua
 url_hdr_alt = 'Mozilla 5.10'
 add_args = {'deviceId':deviceId, 'uuid':getUUID()}
-data = urllib.urlencode(add_args)
+data = uparse.urlencode(add_args)
 #sp_fqdn = "http://api.auth.adobe.com/api/v1/"
 auth_token = 'authToken.txt'
 add_params = {'deviceId':deviceId, 'requestor':requestor_id, 'resource': 'fox'}
 add_params_mrss = {'deviceId':deviceId, 'requestor':requestor_id, 'resource':mrss_res}
-data_authz = urllib.urlencode(add_params)
-data_authz_mrss = urllib.urlencode(add_params_mrss)
+data_authz = uparse.urlencode(add_params)
+data_authz_mrss = uparse.urlencode(add_params_mrss)
         
 
 # get authn token (do 2nd Screen authentication before this)
@@ -86,128 +95,130 @@ data_authz_mrss = urllib.urlencode(add_params_mrss)
 def checkauthn():
         #If token found proceed for next calls else ask to authenticate first
         url3 = str(sp_fqdn) + str("checkauthn.json?requestor=") + str(requestor_id)+ str('&deviceId=sanjeev') # str('&')+str(data)
-        print url3
-        request3 = urllib2.Request(url3)
+        print (url3)
+        request3 = urllib.request.Request(url3)
         request3.add_header('User-agent', url_header)
         request3.add_header('Authorization',theheader)
         
         try:
-                response3 = urllib2.urlopen(request3)
-                print response3.info()
+                response3 = urllib.request.urlopen(request3)
+                print (response3.info())
                 html = response3.read()
-                print html
+                print (html)
 
-        except URLError, g:
-                print g.code
-                print g.reason
-                print g.read() #.split('<message>')[1]
-                print '-->>> Run the auth flow first'
+        except URLError as g:
+                print (g.code)
+                print (g.reason)
+                print (g.read()) #.split('<message>')[1]
+                print ('-->>> Run the auth flow first')
         
         response3.close()
         if response3.code == 200:
-                print '-->>> REST OF the flow is running now\n'
+                print ('-->>> REST OF the flow is running now\n')
                 authorize()
         elif response3.code == 403:
-                print '403? Got it!'
+                print ('403? Got it!')
         else:
-                print 'WHOA'
+                print ('WHOA')
 
 def authorize():
         # retreive authn token
         url4 = str(sp_fqdn) + str("tokens/authn.json?requestor=")+ str(requestor_id)+ str('&deviceId=sanjeev')
-        print url4+'\n'
-        request4 = urllib2.Request(url4)
+        print (url4+'\n')
+        request4 = urllib.request.Request(url4)
         request4.add_header('User-agent', url_header)
         request4.add_header('Authorization',theheader)
 
         try:
-                response4 = urllib2.urlopen(request4)
+                response4 = urllib.request.urlopen(request4)
                 html4 = response4.read()
                 #auth_token = html4
-                print '[Authentication Token]:\n'+ html4
+                print ('[Authentication Token]:\n')
+                print (html4)
        
-        except URLError, g:
-                print g.code
-                print g.reason
-                print g.read().split('<message>')[1]
+        except URLError as g:
+                print (g.code)
+                print (g.reason)
+                print (g.read().split('<message>')[1])
 
         # authorize
         url_authz= str(sp_fqdn) + str('authorize.json?') + str(data_authz_mrss)
-        print '\n'+url_authz
-        req_authz = urllib2.Request(url_authz)
+        print ('\n'+url_authz)
+        req_authz = urllib.request.Request(url_authz)
         req_authz.add_header('User-agent', url_header)
         req_authz.add_header('Authorization',theheader)
 
         try:
-                response_authz = urllib2.urlopen(req_authz)
+                response_authz = urllib.request.urlopen(req_authz)
                 html4_ = response_authz.read()
-                print html4_
+                print (html4_)
         
-        except URLError, z:
-                print z.code
-                print z.reason
-                print z.read().split('<message>')[1]
+        except URLError as z:
+                print (z.code)
+                print (z.reason)
+                print (z.read().split('<message>')[1])
         
         #get authz token
 
         url_rtr_authz = str(sp_fqdn) + str('tokens/authz.json?') + str(data_authz_mrss)
-        print url_rtr_authz+'\n'
-        req_authz2 = urllib2.Request(url_rtr_authz)
+        print (url_rtr_authz+'\n')
+        req_authz2 = urllib.request.Request(url_rtr_authz)
         req_authz2.add_header('User-agent', url_header)
         req_authz2.add_header('Authorization',theheader)
 
         try:
-                response_authz2 = urllib2.urlopen(req_authz)
+                response_authz2 = urllib.request.urlopen(req_authz)
                 html5 = response_authz2.read()
-                print '[Authorization Token]:\n'+ html5
+                print ('[Authorization Token]:\n')
+                print (html5)
 
-        except URLError, e:
-                print e.code
-                print e.reason
-                print e.read().split('<message>')[1]
+        except URLError as e:
+                print (e.code)
+                print (e.reason)
+                print (e.read().split('<message>')[1])
 
         if response_authz2.code == 200:
-                print '\n-->>> Getting Media Token\n'
+                print ('\n-->>> Getting Media Token\n')
                 getMediaToken()
 
 def getMediaToken():
         #retrieve media token
         url_media = str(sp_fqdn) + str('tokens/media.json?') + str(data_authz_mrss)
-        print url_media+'\n'
-        req_media = urllib2.Request(url_media)
+        print (url_media+'\n')
+        req_media = urllib.request.Request(url_media)
         req_media.add_header('User-agent', url_header)
         req_media.add_header('Authorization',theheader)
 
         try:
-                response_media = urllib2.urlopen(req_media)
+                response_media = urllib.request.urlopen(req_media)
                 html6 = response_media.read()
-                print html6
-        except URLError, j:
-                print j.code
-                print j.reason
-                print j.read()
+                print (html6)
+        except URLError as j:
+                print (j.code)
+                print (j.reason)
+                print (j.read())
 
 def getUserMetadata():
         #get user metadata
         meta_params = {'deviceId':deviceId, 'requestor':requestor_id}
-        meta_data = urllib.urlencode(meta_params)
+        meta_data = uparse.urlencode(meta_params)
         url_meta = str(sp_fqdn) + str('tokens/usermetadata.json?') + str(meta_data)
-        req_meta = urllib2.Request(url_meta)
+        req_meta = urllib.request.Request(url_meta)
         req_meta.add_header('User-agent', url_header)
         req_meta.add_header('Authorization',theheader)
         req_meta.add_header('Connection','keep-alive')
         req_meta.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
         
         try:
-                meta_response = urllib2.urlopen(req_meta)
+                meta_response = urllib.request.urlopen(req_meta)
                 html7 = meta_response.read()
-                print '\n-->>> Getting User Metadata now'
-                print '\n'+url_meta+'\n'
-                print html7
-        except URLError, i:
-                print i.code
-                print i.reason
-                print i.read()
+                print ('\n-->>> Getting User Metadata now')
+                print ('\n'+url_meta+'\n')
+                print (html7)
+        except URLError as i:
+                print (i.code)
+                print (i.reason)
+                print (i.read())
 
 
 checkauthn()
