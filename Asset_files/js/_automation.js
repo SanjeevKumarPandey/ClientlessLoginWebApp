55a64ca8f3f56656bf0ecf28ce21a19d9729ae01
Sanjeev Kumar Pandey <sanpande@adobe.com>
2019-05-12 17:45:44 +0530

    adding hulk.min.js

---
+++ Asset_files/js/_automation.js
@@ -0,0 +1,24 @@
+function automation() {
+    $('#formAutomation').css('display', 'block');
+    configLoader('files','listName','byte_content','byte_range');
+}
+
+function launcher(config) {
+//console.log(__data__.requestor, __data__.resource);
+  //console.log(__data__.deviceData.deviceId, __data__.redirectUrl, __data__.provider);
+  // console.log(typeof(result));
+
+
+
+}
+
+
+// function displayForm() {
+//   $('#formAutomation').css('display', 'block');
+//   document.getElementById('files').addEventListener('change', handleFileSelect, false);
+// }
+//outputElement = l;//'listName';
+  //textByteContent = b;//'byte_content';
+  //fileSizeReader = f;//'byte_range';
+  //file = _files;
+  //displayForm();
